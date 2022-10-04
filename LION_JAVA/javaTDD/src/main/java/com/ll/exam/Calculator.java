f15feede441a51ebe4cfd3d9d1927671096ad8a5
shinyehwan <jghisjl007@naver.com>
2022-10-05 00:44:24 +0900

    add 함수명을 run로 수정

--- LION_JAVA/javaTDD/src/main/java/com/ll/exam/Calculator.java
+++ LION_JAVA/javaTDD/src/main/java/com/ll/exam/Calculator.java
@@ -1,7 +1,7 @@
 package com.ll.exam;
 
 public class Calculator {
-    public static int add(String s) {
+    public static int run(String s) {
 //        if (s.equals("10+20")) {
 //            return 30;
 //        }
--- LION_JAVA/javaTDD/src/test/java/com/ll/exam/Apptest.java
+++ LION_JAVA/javaTDD/src/test/java/com/ll/exam/Apptest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Apptest {
     @Test
-    public void 더하기_테스트() {
+    public void 더하기_테스트0() {
         int a = 10 + 5;
         assertEquals(15, a);
 
@@ -15,19 +15,19 @@ public class Apptest {
 
     @Test
     public void 더하기_테스트2() {
-        int rs = Calculator.add("10+20");
+        int rs = Calculator.run("10+20");
         assertEquals(30, rs);
     }
 
     @Test
     public void 더하기_테스트3() {
-        int rs = Calculator.add("20+20");
+        int rs = Calculator.run("20+20");
         assertEquals(40, rs);
     }
 
     @Test
     public void 더하기_테스트4() {
-        int rs = Calculator.add("10+10");
+        int rs = Calculator.run("10+10");
         assertEquals(20, rs);
     }
 
