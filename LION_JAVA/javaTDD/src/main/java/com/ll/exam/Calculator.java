4648fd0680ed7ba12d21c2b5b35b9c119eececd4
shinyehwan <jghisjl007@naver.com>
2022-10-05 01:15:20 +0900

    곱하기 기능 구현

--- LION_JAVA/javaTDD/src/main/java/com/ll/exam/Calculator.java
+++ LION_JAVA/javaTDD/src/main/java/com/ll/exam/Calculator.java
@@ -1,28 +1,40 @@
 package com.ll.exam;
 
 public class Calculator {
-    public static int run(String s) {
+    public static int calc(String s) {
 //        if (s.equals("10+20")) {
 //            return 30;
 //        }
 
+        boolean isMulti = s.indexOf("*") != -1;
         boolean isMinus = s.indexOf("-") != -1;
 
-        if (isMinus) {
-            return runMinus(s);
-        }
 
-        return runPlus(s);
+
+        if (isMinus) {
+            return calcMinus(s);
+        } else if (isMulti) {
+            return calcMulti(s);
+
+        }
+        return calcPlus(s);
     }
 
-    private static int runPlus(String s) {
+    private static int calcMulti(String s) {
+        String[] sBits = s.split("\\*");
+        int no1 = Integer.parseInt(sBits[0]);
+        int no2 = Integer.parseInt(sBits[1]);
+        return no1 * no2;
+    }
+
+    private static int calcPlus(String s) {
         String[] sBits = s.split("\\+");
         int no1 = Integer.parseInt(sBits[0]);
         int no2 = Integer.parseInt(sBits[1]);
         return no1 + no2;
     }
 
-    private static int runMinus(String s) {
+    private static int calcMinus(String s) {
         String[] sBits = s.split("\\-");
         int no1 = Integer.parseInt(sBits[0]);
         int no2 = Integer.parseInt(sBits[1]);
--- LION_JAVA/javaTDD/src/test/java/com/ll/exam/Apptest.java
+++ LION_JAVA/javaTDD/src/test/java/com/ll/exam/Apptest.java
@@ -15,26 +15,33 @@ public class Apptest {
 
     @Test
     public void 더하기_테스트2() {
-        int rs = Calculator.run("10+20");
+        int rs = Calculator.calc("10+20");
         assertEquals(30, rs);
     }
 
     @Test
     public void 더하기_테스트3() {
-        int rs = Calculator.run("20+20");
+        int rs = Calculator.calc("20+20");
         assertEquals(40, rs);
     }
 
     @Test
     public void 더하기_테스트4() {
-        int rs = Calculator.run("10+10");
+        int rs = Calculator.calc("10+10");
         assertEquals(20, rs);
     }
 
     @Test
     public void 빼기() {
-        int rs = Calculator.run("10-10");
+        int rs = Calculator.calc("10-10");
         assertEquals(0, rs);
     }
+
+    @Test
+    public void 곱하기() {
+        int rs = Calculator.calc("10*10");
+        assertEquals(100, rs);
+    }
+
 
 }
